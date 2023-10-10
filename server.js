54081fa32deda686d2c60d4656da3b514902c56f
wechu07 <wechu017@gmail.com>
2023-10-10 23:33:56 +0300

    shrink first demo

--- server.js
+++ server.js
@@ -39,6 +39,10 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/donate", (req, res) => {
+  res.render("donate");
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
